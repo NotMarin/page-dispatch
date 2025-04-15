1d1c6b7d8193aa316211a209c8048635b4b1e65b
Santiago Marin <santimarinh04@gmail.com>
2025-04-15 16:37:20 -0500

    feat: shadcn and theme config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { useTheme } from "next-themes";
+
 export default function Home() {
+  const { setTheme } = useTheme();
   return (
     <div>
       <p>Esta es la app</p>
+      <button onClick={() => setTheme("dark")}>Dark</button>
+      <button onClick={() => setTheme("light")}>Light</button>
     </div>
   );
 }
